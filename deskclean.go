ca3294463f5d44c2c6398c3adf27c54814390a8a
Austin Davis-Richardson <harekrishna@gmail.com>
2018-02-20 22:11:40 -0800

    go fmt

--- deskclean.go
+++ deskclean.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "log"
-  "path"
-  "os/user"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+	"path"
 )
 
 func getHomeDirectory() string {
-  usr, _ := user.Current()
-  return usr.HomeDir
+	usr, _ := user.Current()
+	return usr.HomeDir
 }
 
 // check if path is directory
 func isDirectory(filepath string) bool {
-  file, err := os.Stat(filepath)
+	file, err := os.Stat(filepath)
 
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  return file.Mode().IsDir()
+	return file.Mode().IsDir()
 }
 
 func main() {
 
-  home := getHomeDirectory()
+	home := getHomeDirectory()
 
-  fmt.Println(home)
+	fmt.Println(home)
 
-  files, err := ioutil.ReadDir(path.Join(home, "Desktop"))
+	files, err := ioutil.ReadDir(path.Join(home, "Desktop"))
 
-  if err != nil {
-    log.Fatal(err)
-  }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  for _, f := range files {
-    filepath := path.Join(home, "Desktop", f.Name())
-    fmt.Println(filepath)
+	for _, f := range files {
+		filepath := path.Join(home, "Desktop", f.Name())
+		fmt.Println(filepath)
 
-    if isDirectory(filepath) {
-      fmt.Println(filepath)
-    }
+		if isDirectory(filepath) {
+			fmt.Println(filepath)
+		}
 
-  }
+	}
 }
