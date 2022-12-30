02548ee53e9a68acf6c4dba6b9c1234376206646
Austin Davis-Richardson <harekrishna@gmail.com>
2022-12-29 16:50:46 -0800

    Update formula for 0.1.2

--- formula.rb
+++ formula.rb
@@ -1,9 +1,9 @@
 class Deskclean < Formula
   desc "Keep your ~/Desktop tidy!"
   homepage "https://github.com/audy/deskclean"
-  url "https://github.com/audy/deskclean/archive/refs/tags/0.1.1.tar.gz"
-  version "0.0.1"
-  sha256 ""
+  url "https://github.com/audy/deskclean/archive/refs/tags/0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "3249b6553492ba3097f1a24da3ba936b81596bd2"
   license "MIT"
 
   depends_on "go" => :build
